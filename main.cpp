2658717dd6391ad7ad3906fddad836087b615e61
manuelpc97 <manuelpc97@unitec.edu>
2016-03-11 13:46:50 -0600

    Agrega e imprimer racionales

--- main.cpp
+++ main.cpp
@@ -9,12 +9,16 @@ using std::cout;
 using std::cin;
 using std::string;
 
+void mostrarRacionales(vector<racional>& );
+
 int main(int argc, char*argv[]){
 	vector <racional> racionales;
 	vector <string> historial;
+	racional fraccion(1,1);
 	bool seguir = true;
 	int decision = 0;
-
+	int numerador = 0, denominador = 0;
+	
 	while(seguir){
 		cout<<"------------------Menu-------------------"<<endl;
 		cout<<"1. Crear racionales. "<<endl;
@@ -27,9 +31,23 @@ int main(int argc, char*argv[]){
 		cin>>decision;
 
 		if(decision ==1){
-
+			while(denominador == 0){
+				cout<<"Ingrese el numerador: ";
+				cin>>numerador;
+				cout<<"Ingrese el denominador: ";
+				cin>>denominador;
+				
+				if(denominador == 0){
+					cout<<"ERROR! Su denominador no puede ser cero"<<endl;
+					cout<<"Ingrese todos los datos de nuevo "<<endl;
+				}else{
+					fraccion = racional(numerador,denominador);
+					racionales.push_back(fraccion);									
+				}		
+			}
+			denominador = 0;		
 		}else if(decision == 2){
-
+			mostrarRacionales(racionales);
 		}else if(decision == 3){
 		
 		}else if(decision == 4){
@@ -45,3 +63,10 @@ int main(int argc, char*argv[]){
 		
 	return 0;
 }
+
+void mostrarRacionales(vector<racional>& lista){
+	for(int i = 0; i < lista.size(); i++){
+		cout<<"Posicion: "<< i <<lista[i].toString()<<endl;
+	}
+}
+
